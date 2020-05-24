09fc1f700d1cfb1e0fe1e6f1fc69732a19d5681d
Alexander Neupokoev <admin@armavir.ru>
2020-05-24 20:40:25 +0300

    Change innerHTML to createElement

--- 04_lesson/fcs/flightDetails.js
+++ 04_lesson/fcs/flightDetails.js
@@ -8,24 +8,29 @@ function flightDetails(flightName) {
         return;
     }
 
+    const divContainer = document.getElementById('flight-details')
+    const headHTML = document.createElement('h1')
+    headHTML.textContent = `Отчет по рейсу ${flightName}`
+    divContainer.append(headHTML)
+
     let infoHTML = document.createElement('div')
-    infoHTML.id = 'flight-details'
-    infoHTML.innerHTML = (`<h1>Отчет по рейсу ${flightName}</h1>`)
+
     for (let ticket of flight.tickets) {
         let statusReg
         ticket.registrationTime === null ? statusReg = 'не пройдена' : statusReg = 'не пройдена'
-        infoHTML.innerHTML = (`
-        <h1>Отчет по рейсу ${flightName}</h1>
-        <div>Номер билета: ${ticket.id}</div>
-        <div>Место: ${ticket.seat}</div>
-        <div>Пассажир: ${ticket.fullName}</div>
-        <div>Регистрация: ${statusReg}</div>
-        <div><hr></div>
-        `)
+        const ticketHTML = document.createElement('div')
+        ticketHTML.textContent = `Номер билета: ${ticket.id}`
+        divContainer.append(ticketHTML)
+        const seatHTML = document.createElement('div')
+        seatHTML.textContent = `Место: ${ticket.seat}`
+        divContainer.append(seatHTML)
+        const nameHTML = document.createElement('div')
+        nameHTML.textContent = `Пассажир: ${ticket.fullName}`
+        divContainer.append(nameHTML)
+        const regHTML = `Регистрация: ${statusReg}`
+        divContainer.append(regHTML)
     }
 
-    console.log('----')
-    document.body.prepend(infoHTML)
 }
 
 flightDetails('BH118')
